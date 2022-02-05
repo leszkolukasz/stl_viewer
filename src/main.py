480a51a86280cb451185fbaf61b651ec2b8a52bf
Łukasz Leszko <leszko.lucas@gmail.com>
2022-02-05 20:54:20 +0100

    Add functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.filedialog
+
 from render.render_manager import RenderManager
+from mesh import Mesh
 
 class App:
     
@@ -9,28 +11,31 @@ class App:
         self.root.title("STL Viewer")
         self.root.geometry("600x600")
         
-        self.mesh = None
+        self.mesh = Mesh()
         self.mode = tk.StringVar(value="dotted")
         self.resolution = tk.IntVar(value=10)
         
-        self.render_manager = RenderManager(self.root, None, None, self.mode, self.resolution)
-        self._build_ui()
-        self.render_manager.ascii_view = self.ascii_view
-        self.render_manager.dotted_view = self.dotted_view
+        self._build_ui_frames()
+        self.render_manager = RenderManager(self.root, self.mesh, self.dotted_view, self.dotted_view, self.mode, self.resolution)
+        self._build_ui_widgets()
         
     def run(self):
         self.root.mainloop()
         
-    def _build_ui(self):
+    def _build_ui_frames(self):
         self.root.columnconfigure(0, weight=1)
         for i in range(21):
             self.root.rowconfigure(i, weight=1)
 
-        self.render_frame = tk.Frame(self.root, bg='red')
+        self.render_frame = tk.Frame(self.root, bg='red', padx=10, pady=10)
         self.render_frame.grid(row=0, column=0, rowspan=20, sticky='nswe')
         
-        self.dotted_view = tk.Canvas()
-        self.ascii_view = tk.Canvas()
+        self.render_frame.rowconfigure(0, weight=1)
+        self.render_frame.columnconfigure(0, weight=1)
+        
+        self.dotted_view = tk.Canvas(self.render_frame, background='black')
+        self.dotted_view.grid(row=0, column=0, sticky='nswe')
+        self.ascii_view = tk.Canvas(self.render_frame, background='white')
 
         self.nav_frame = tk.Frame(self.root,  bg='green', padx=10)
         self.nav_frame.grid(row=20, column=0, sticky='nswe')
@@ -43,6 +48,7 @@ class App:
         self.nav_frame.columnconfigure(4, weight=1)
         self.nav_frame.columnconfigure(5, weight=1)
 
+    def _build_ui_widgets(self):
         tk.Button(self.nav_frame, text="Open", command=self.open_file, pady=10).grid(row=0, column=0, sticky='nswe')
         tk.Label(self.nav_frame, text="View mode: ").grid(row=0, column=1, sticky='nswe')
         tk.Radiobutton(self.nav_frame, text='Dotted', value='dotted', variable=self.mode, command=self.render_manager.change_mode).grid(row=0, column=2, sticky='nswe')
@@ -51,8 +57,12 @@ class App:
         tk.Scale(self.nav_frame, orient=tk.HORIZONTAL, length=200, from_=1.0, to=100.0, variable=self.resolution, command=self.render_manager.change_resolution).grid(row=0, column=5, sticky='nswe')
         
     def open_file(self):
-        path = tk.filedialog.askopenfilename()
-        print(path)
+        #path = tk.filedialog.askopenfilename()
+        path = '/home/whistleroosh/Desktop/cube.stl'
+        print(self.dotted_view.winfo_width())
+        if len(path):
+            self.mesh.load_file(path)
+            self.render_manager.render()
 
 if __name__ == '__main__':
     app = App()
