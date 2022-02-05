4f783e2c83633de1c24d851d4d4722c0df29ed21
Łukasz Leszko <leszko.lucas@gmail.com>
2022-02-05 22:40:59 +0100

    Chaneged points calculation

--- src/mesh.py
+++ src/mesh.py
@@ -16,15 +16,16 @@ class Mesh():
         if self.points is not None:
             return self.points
         
-        self.points = np.zeros((3*len(self.mesh.points), 3))
+        """self.points = np.zeros((3*len(self.mesh.points), 3))
         for i in range(len(self.mesh.points)):
-            """self.points[i, 0] = np.sum(self.mesh.points[i, ::3])
+            self.points[i, 0] = np.sum(self.mesh.points[i, ::3])
             self.points[i, 1] = np.sum(self.mesh.points[i, 1::3])
             self.points[i, 2] = np.sum(self.mesh.points[i, 2::3])
-            self.points[i] /= 3"""
+            self.points[i] /= 3
             self.points[3*i] = self.mesh.points[i, 0:3]
             self.points[3*i+1] = self.mesh.points[i, 3:6]
-            self.points[3*i+2] = self.mesh.points[i, 6:9]
+            self.points[3*i+2] = self.mesh.points[i, 6:9]"""
+        self.points = self.mesh.points
 
         return self.points
     
