1144b146bf507f29d492aa76afd23de89e05c372
Łukasz Leszko <leszko.lucas@gmail.com>
2022-02-07 20:07:05 +0100

    Update point getting function

--- src/mesh.py
+++ src/mesh.py
@@ -11,20 +11,11 @@ class Mesh():
     def load_file(self, path):
         self.mesh = stl.mesh.Mesh.from_file(path)
         
-    def get_points(self, x, y):      
-        """self.points = np.zeros((3*len(self.mesh.points), 3))
-        for i in range(len(self.mesh.points)):
-            self.points[i, 0] = np.sum(self.mesh.points[i, ::3])
-            self.points[i, 1] = np.sum(self.mesh.points[i, 1::3])
-            self.points[i, 2] = np.sum(self.mesh.points[i, 2::3])
-            self.points[i] /= 3
-            self.points[3*i] = self.mesh.points[i, 0:3]
-            self.points[3*i+1] = self.mesh.points[i, 3:6]
-            self.points[3*i+2] = self.mesh.points[i, 6:9]"""
-        if x != 0:
-            self.mesh.rotate([0, 1, 0], -x/100)
-        if y != 0:
-            self.mesh.rotate([1, 0, 0], -y/100)
+    def get_points(self, rotate_x, rotate_y):
+        if rotate_x != 0:
+            self.mesh.rotate([0, 1, 0], rotate_x/100)
+        if rotate_y != 0:
+            self.mesh.rotate([1, 0, 0], rotate_y/100)
 
         return self.mesh.points
     
